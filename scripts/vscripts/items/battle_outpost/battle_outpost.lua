c62fbaf2aff229cf95e21b40271bc9be9c9c904b
Mit <719505697@qq.com>
2019-12-08 22:53:23 +0800

    战斗前哨判断两队人数

--- scripts/vscripts/items/battle_outpost/battle_outpost.lua
+++ scripts/vscripts/items/battle_outpost/battle_outpost.lua
@@ -1,10 +1,22 @@
---[[
-function exp_up(keys)
+function get_team_point(keys)
     local target_entities = keys.target_entities
+    local good = 0
+    local bad = 0
     for _, target in ipairs(target_entities) do
-        local level = target:GetLevel()
-        local xp = 10.0 + level * 2
-        target:AddExperience(xp, 0, false, false)
+        local team = target:GetTeam()
+        if team then
+            if team == DOTA_TEAM_GOODGUYS then
+                good = good + 1
+            elseif team == DOTA_TEAM_BADGUYS then
+                bad = bad + 1
+            end
+        end
+    end
+    if good == bad then
+        print("good=bad")
+    elseif good < bad then
+        print("good<bad")
+    elseif good > bad then
+        print("good>bad")
     end
 end
---]] 
