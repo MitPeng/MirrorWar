d4a7ab1fde6606efdd02513d698192c6a36f019f
Mit <719505697@qq.com>
2019-12-16 20:55:00 +0800

    灵魂汲取技能效果

---
+++ scripts/vscripts/heroes/immortal_soul/soul_draw.lua
@@ -0,0 +1,17 @@
+function projectile_hit(keys)
+    local caster = keys.caster
+    local target = keys.target
+    local ability = keys.ability
+    local int_mul = ability:GetSpecialValueFor("intellect_multiple")
+    local damage = ability:GetLevelSpecialValueFor("damage",
+                                                   ability:GetLevel() - 1) +
+                       caster:GetIntellect() * int_mul
+    damage_table = {
+        victim = target,
+        attacker = caster,
+        damage = damage,
+        damage_type = DAMAGE_TYPE_MAGICAL
+    }
+    ApplyDamage(damage_table)
+    caster:Heal(damage / 10, caster)
+end
