42d27abb5f6d25caaa14780d4e3bf70eafc2c987
Mit <719505697@qq.com>
2019-12-24 13:29:44 +0800

    血链鞭挞群体拉进效果及中英文说明

--- scripts/vscripts/heroes/blood_chain/blood_chain_flog.lua
+++ scripts/vscripts/heroes/blood_chain/blood_chain_flog.lua
@@ -4,40 +4,53 @@ function init_value(keys)
     local ability = keys.ability
     local caster_location = caster:GetAbsOrigin()
     local target_location = target:GetAbsOrigin()
-    ability.ability_distance = (target_location - caster_location):Length2D()
-    ability.ability_duration = ability:GetLevelSpecialValueFor("duration",
-                                                               ability:GetLevel() -
-                                                                   1)
-    ability.ability_direction = (caster_location - target_location):Normalized()
-    ability.ability_speed = (ability.ability_distance - 150) /
-                                (ability.ability_duration * 30)
-    ability.ability_save_distance = 0
+    target.ability_damage = ability:GetLevelSpecialValueFor("damage",
+                                                            ability:GetLevel() -
+                                                                1) / 30
+    target.ability_distance = (target_location - caster_location):Length2D()
+    target.ability_duration = ability:GetLevelSpecialValueFor("duration",
+                                                              ability:GetLevel() -
+                                                                  1)
+    target.ability_direction = (caster_location - target_location):Normalized()
+    target.ability_speed = (target.ability_distance - 150) /
+                               (target.ability_duration * 30)
+    target.ability_save_distance = 0
 end
 
 function target_motion(keys)
     local caster = keys.caster
     local target = keys.target
-    local ability = keys.ability
-    if ability.ability_distance < 150 then
-        -- target:InterruptMotionControllers(false)
+    if target.ability_distance < 150 then
         return
     else
-        if ability.ability_save_distance + 150 < ability.ability_distance then
+        if target.ability_save_distance + 150 < target.ability_distance then
             local target_origin = target:GetAbsOrigin() +
-                                      ability.ability_direction *
-                                      ability.ability_speed
+                                      target.ability_direction *
+                                      target.ability_speed
             target:SetAbsOrigin(target_origin)
-            -- FindClearSpaceForUnit(target, target_origin, false)
-            ability.ability_save_distance =
-                ability.ability_save_distance + ability.ability_speed
+            target.ability_save_distance =
+                target.ability_save_distance + target.ability_speed
         else
-            -- target:InterruptMotionControllers(false)
             return
         end
     end
 
 end
 
-function aoe_motion(keys)
-    -- body
-end
+-- function aoe_motion(keys)
+--     local caster = keys.caster
+--     local target = keys.target
+--     local ability = keys.ability
+--     local ability_target_location = ability.ability_target:GetAbsOrigin()
+--     local target_location = target:GetAbsOrigin()
+--     local direction = (ability_target_location - target_location):Normalized()
+--     local distance = (ability_target_location - target_location):Length2D()
+--     local speed = distance / 50
+--     -- 距离大于0则继续牵引
+--     if distance > 50 then
+--         local target_origin = target_location + direction * speed
+--         target:SetAbsOrigin(target_origin)
+--     else
+--         return
+--     end
+-- end
