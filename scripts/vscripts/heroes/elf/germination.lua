b5f6a1329c927184dcaab61316049e9f6b774174
Mit <719505697@qq.com>
2020-01-07 20:30:49 +0800

    添加回春技能及说明，修改萌芽技能

--- scripts/vscripts/heroes/elf/germination.lua
+++ scripts/vscripts/heroes/elf/germination.lua
@@ -1,18 +1,36 @@
 function germination(keys)
     local caster = keys.caster
-    local point = keys.target_points[1]
+    local point = caster:GetAbsOrigin()
     local ability = keys.ability
+    local targetTeam = ability:GetAbilityTargetTeam()
+    local targetType = ability:GetAbilityTargetType()
+    local targetFlag = ability:GetAbilityTargetFlags()
+    local damageType = ability:GetAbilityDamageType()
     local radius = ability:GetSpecialValueFor("radius")
     local number = ability:GetLevelSpecialValueFor("number",
                                                    ability:GetLevel() - 1)
     local duration = ability:GetLevelSpecialValueFor("duration",
                                                      ability:GetLevel() - 1)
+    local damage = ability:GetLevelSpecialValueFor("damage",
+                                                   ability:GetLevel() - 1)
     for i = 1, number do
-        local distance = RandomInt(0, radius)
+        local distance = RandomInt(100, radius)
         local angle = RandomInt(0, 360)
         local dy = distance * math.sin(angle)
         local dx = distance * math.cos(angle)
         local vec = Vector(point.x + dx, point.y + dy, point.z)
         CreateTempTree(vec, duration)
+    end
+    local units = FindUnitsInRadius(caster:GetTeamNumber(), point, caster,
+                                    radius, targetTeam, targetType, targetFlag,
+                                    0, false)
+    for _, v in pairs(units) do
+        local damageTable = {
+            victim = v,
+            attacker = caster,
+            damage = damage,
+            damage_type = damageType
+        }
+        ApplyDamage(damageTable)
     end
 end
