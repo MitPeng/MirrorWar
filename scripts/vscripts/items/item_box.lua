1efd2eadb5ac6796b0c71f866c82b89cb4ef737b
Mit <719505697@qq.com>
2020-01-14 19:21:17 +0800

    1-5级箱子有几率开出更高一级的箱子

--- scripts/vscripts/items/item_box.lua
+++ scripts/vscripts/items/item_box.lua
@@ -6,4 +6,10 @@ function open_box(keys)
     local number = RandomInt(1, count)
     local item_name = table[tostring(number)]
     local item = caster:AddItemByName(item_name)
+    -- 有几率开出高级箱子
+    if keys.box_number < 6 and RandomInt(1, 100) <=
+        _G.load_kv["open_great_box_percent"] then
+        local great_box = caster:AddItemByName(
+                              "item_box_" .. tostring(keys.box_number + 1))
+    end
 end
