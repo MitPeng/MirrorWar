a15e822d96b9263473fe45c1b7671c460aed563f
Mit <719505697@qq.com>
2020-02-15 11:40:52 +0800

    boss每次出生的等级升高

--- scripts/vscripts/units/battle_outpost/create_boss.lua
+++ scripts/vscripts/units/battle_outpost/create_boss.lua
@@ -3,4 +3,11 @@ function create_boss(keys)
                     :GetOrigin()
     local boss = Utils:create_unit_simple("boss_1", vec, true,
                                           DOTA_TEAM_CUSTOM_1)
+    if keys.ability.boss_lvl ~= nil then
+        boss:CreatureLevelUp(keys.ability.boss_lvl)
+        keys.ability.boss_lvl = keys.ability.boss_lvl + 5
+    else
+        keys.ability.boss_lvl = 4
+        boss:CreatureLevelUp(keys.ability.boss_lvl)
+    end
 end
