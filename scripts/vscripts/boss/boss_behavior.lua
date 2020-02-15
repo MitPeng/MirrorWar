2631ec286d69b8cd549b19aa822ec97d1a011176
Mit <719505697@qq.com>
2020-02-15 19:20:51 +0800

    修bug，改技能伤害

--- scripts/vscripts/boss/boss_behavior.lua
+++ scripts/vscripts/boss/boss_behavior.lua
@@ -1,14 +1,18 @@
 -- 每分钟升1级
 function lvlup(keys)
     local boss = keys.caster
+    local health_percent = boss:GetHealthPercent() / 100
     boss:CreatureLevelUp(1)
+    boss:SetHealth(boss:GetMaxHealth() * health_percent)
 end
 
 -- Boss被击杀给积分
 function death(keys)
     local boss = keys.caster
     local hero = keys.attacker
-    local boss_score = _G.load_kv["base_boss_score"] + boss:GetLevel() *
-                           _G.load_kv["lvl_boss_score"]
-    hero.boss_score = hero.boss_score + boss_score
+    if hero:IsHero() then
+        local boss_score = _G.load_kv["base_boss_score"] + boss:GetLevel() *
+                               _G.load_kv["lvl_boss_score"]
+        hero.boss_score = hero.boss_score + boss_score
+    end
 end
